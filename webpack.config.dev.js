31788d1f234c6a1189d2cfe74007b28330c34128
MaximoReigns <id569723@outlook.com>
2021-03-31 21:05:12 -0500

    Implementación de dependencias: optimización, hashes, compresión y minificación, alias, variables de entorno, modo desarrollo, modo producción, watch y deploy con netlify

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,10 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
 const CopyPlugin = require ('copy-webpack-plugin');
-const CssMinimizerPlugin = require ('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require ('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     // Entry nos permite decir el punto de entrada de nuestra aplicación
@@ -20,6 +17,8 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
+    mode: 'development',
+    watch: true,
     resolve: {
         // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
         extensions: ['.js'],
@@ -99,13 +98,5 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
 }
